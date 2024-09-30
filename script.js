6319a140d197a7937a542a2ed2642402deb32b49
David Hernández <dhernandezla0@gmail.com>
2024-09-30 09:04:42 +0200

    Commit

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
             img.classList.toggle('active');
         });
     });
+
+    const timelineItems = document.querySelectorAll('.timeline-item');
+    let currentIndex = 0
+    const nextDayBtn = document.getElementById('next-day-btn');
+
+    const showNextDay = () => {
+        if (currentIndex < timelineItems.length) {
+            timelineItems[currentIndex].style.display = 'block';
+
+            timelineItems[currentIndex].offsetHeight;
+            timelineItems[currentIndex].classList.add('visible');
+
+            const buttonContainer = document.createElement('div');
+            buttonContainer.classList.add('button-container');
+            buttonContainer.appendChild(nextDayBtn);
+            timelineItems[currentIndex].appendChild(buttonContainer);
+            nextDayBtn.style.display = 'block';
+
+            currentIndex++;
+        } else {
+            nextDayBtn.textContent = 'Todos los días mostrados';
+            nextDayBtn.disabled = true;
+            nextDayBtn.style.display = 'none';
+        }
+    };
+
+    nextDayBtn.addEventListener('click', showNextDay);
+    showNextDay();
 });
